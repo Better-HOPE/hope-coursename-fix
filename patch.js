06a7982b0a9964a6e1eef53a087c98d5a629b0f5
kume-negitoro <kume.negitoro@gmail.com>
2022-04-13 02:12:15 +0900

    fix target elements

--- patch.js
+++ patch.js
@@ -27,7 +27,7 @@ function getCourseRecord () {
 void (async function main () {
     const courseRecord = await getCourseRecord()
 
-    const badCourseNamePattern = /^\d\-.+/
+    const badCourseNamePattern = /^\d{4}\-.+/
     const selector = 'a[href*="https://hope.fun.ac.jp/course/view.php?id="]'
     const anchors = [...document.querySelectorAll(selector)]
 
@@ -35,7 +35,11 @@ void (async function main () {
         const [_, id] = a.href.split('=')
         const targets = Array.from(a.querySelectorAll('* *'))
             .filter(e => e.innerHTML.match(badCourseNamePattern))
-        for (const target of [a, ...targets]) {
+
+        if (a.children.length === 0) {
+            a.innerHTML = courseRecord[id] || a.innerHTML
+        }
+        for (const target of targets) {
             target.innerHTML = courseRecord[id] || target.innerHTML
         }
     }
